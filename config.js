8b2f2f04fdf0dd7c7836bdbf591f47db90a59891
Therapeutcom <apple.gang145@gmail.com>
2024-06-14 21:54:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.15_6_4f51_2e15_b480 || '').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
